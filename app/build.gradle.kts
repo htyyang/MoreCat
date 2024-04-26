5431d821f563a38d898f47696ceef26b58ce18fe
Haotian Yang <hyang57@u.rochester.edu>
2024-04-25 21:55:18 -0400

    Allow Internet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,16 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // coil
+    implementation("io.coil-kt:coil-compose:2.6.0")
+
+    // retrofit
+    val retrofitVersion = "2.11.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-scalars:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+
+    // gson
+    implementation("com.google.code.gson:gson:2.10.1")
 }
